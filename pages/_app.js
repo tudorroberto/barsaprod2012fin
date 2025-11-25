26819979211b17cc8fc07b4d2da0421aa7e649ab
tudorroberto <tudorroberto@gmail.com>
2025-11-25 09:56:22 +0100

    Add favicon to website

--- pages/_app.js
+++ pages/_app.js
@@ -1,2 +1,14 @@
 import '../styles/globals.css'
-export default function App({ Component, pageProps }) { return <Component {...pageProps} /> }
+import Head from 'next/head'
+
+export default function App({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="/favicon.png" />
+                <title>Barsa Prod 2012</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
+}
